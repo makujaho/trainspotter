e7c6412e7f97b9c63e518ca228fe1bca5e1c6951
nuit <nuit@hoeja.de>
2014-09-07 01:27:26 +0200

    fixed timestamp encoding of coords

--- trainspotter.py
+++ trainspotter.py
@@ -6,18 +6,20 @@ from trainspotter   import train
 from datetime       import datetime
 
 def getTrains():
-    time=datetime.now().strftime('%H:%M:%S')
+    time = datetime.now().strftime('%H:%M:%S')
+    date = datetime.now().strftime('%Y%m%d')
     for i in range(1,24):
-        r = requests.get('http://www.apps-bahn.de/bin/livemap/query-livemap.exe/dny?L=vs_livefahrplan&performLocating=1&performFixedLocating='+str(i)+'&look_requesttime='+time+'&livemapRequest=no&ts=20140710')
-        a = r.json()
-        try:
-            for i in a[0][:-1]:
-                if i[6] != '':
+        while True:
+            r = requests.get('http://www.apps-bahn.de/bin/livemap/query-livemap.exe/dny?L=vs_livefahrplan&performLocating=1&performFixedLocating='+str(i)+'&look_requesttime='+time+'&livemapRequest=no&ts='+date)
+            a = r.json()
+            try:
+                for i in a[0][:-1]:
                     p = train.Train(i, utils.calcCKV(a[0][-1][5], len(a[0])-1))
                     print(p)
-        except Exception as e:
-            print(e)
-            pass
+                else:
+                    break
+            except Exception as e:
+                pass
 
 getTrains()
 
