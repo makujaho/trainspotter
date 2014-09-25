155363a3705a6a852cd68a778efe679f7e905079
MKzero <info@linux-web-development.de>
2014-09-25 09:29:00 +0200

    Indentation and fix for dict access

--- trainspotter/elasticsearch.py
+++ trainspotter/elasticsearch.py
@@ -2,16 +2,16 @@ from elasticsearch import Elasticsearch
 
 class ElasticSearch:
     def __init__(self, host, port, index, doc_type):
-        self.host = host
-        self.port = port
-        self.index = index
-        self.es_type = doc_type
+        self.host       = host
+        self.port       = port
+        self.index      = index
+        self.doc_type   = doc_type
 
         self.es = Elasticsearch()
 
     def index(self, data):
-        es_id = "%s-%s-%s-%s-%s" % (data.dateRef, data.time, data.x, data.y, 
-                                 data.id)
+        es_id = "%s-%s-%s-%s-%s" % (data['dateRef'], data['time'], data['x'],
+                                    data['y'], data['id'])
         r = self.es.index(index=self.index, doc_type=self.doc_type, id=es_id,
                           body=data)
         return r
