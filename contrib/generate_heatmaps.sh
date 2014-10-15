b5ac0defb96f5241ab4cc0217777c242df35d0bb
Jan Hoersch <nuit@hoeja.de>
2014-10-15 13:50:39 +0200

    generate_heatmap wants base directory as first argument

--- contrib/generate_heatmaps.sh
+++ contrib/generate_heatmaps.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 #
-# This file takes JSON dumps with train data in data/ and converts them to 
-# PNG-images in img/ with heatmaps(using heatmap.py)
+# This file takes JSON dumps with train data and converts them to 
+# PNG-images in the same folder as the data with heatmaps(using heatmap.py)
 #
 
-test -d ../img || mkdir ../img
-
-for i in ../data/alltrains-*
+for i in $(find "${1}" -name "*.log");
 do
 	IMG_FILE=$(echo "$i" | sed -e 's:data:img:' -e 's:.log:.png:')
 	if [ -e "$IMG_FILE" ]
 	then
 		echo "$IMG_FILE already exists."
 	else
-		echo "Converting $i to image"
+		echo "Converting $i to $IMG_FILE"
 		python heatmap.py $i "$IMG_FILE"
 	fi
 done
