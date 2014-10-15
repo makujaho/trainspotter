e1f8ae0f0e5f40e3a682aee7a481aa50ab416694
Jan Hoersch <nuit@hoeja.de>
2014-10-15 13:50:51 +0200

    fixed whitespace error...

--- contrib/generate_heatmaps.sh
+++ contrib/generate_heatmaps.sh
@@ -6,12 +6,12 @@
 
 for i in $(find "${1}" -name "*.log");
 do
-	IMG_FILE=$(echo "$i" | sed -e 's:data:img:' -e 's:.log:.png:')
-	if [ -e "$IMG_FILE" ]
-	then
-		echo "$IMG_FILE already exists."
-	else
-		echo "Converting $i to $IMG_FILE"
-		python heatmap.py $i "$IMG_FILE"
-	fi
+    IMG_FILE=$(echo "$i" | sed -e 's:data:img:' -e 's:.log:.png:')
+    if [ -e "$IMG_FILE" ]
+    then
+        echo "$IMG_FILE already exists."
+    else
+        echo "Converting $i to $IMG_FILE"
+        python heatmap.py $i "$IMG_FILE"
+    fi
 done
