3c4825708c92e92c2570d6634356f1bf1dbc298f
mkzero <info@linux-web-development.de>
2015-09-05 02:51:01 +0200

    Added coordinate + productclass conversion functions to utils

--- trainspotter/utils.py
+++ trainspotter/utils.py
@@ -9,3 +9,18 @@ def buildUrl(zoom=False, pos_x=0, pos_y=0):
         return ""
     else:
         return ""
+
+def convert_coord(coord):
+    return coord/1000000.0
+
+def get_productclass_name(productclass):
+    return {
+        1: 'ICE',
+        2: 'IC / EC / CNL',
+        4: 'EN',
+        8: 'RB / RE',
+        16: 'S',
+        16386: 'EC',
+        16388: 'EC',
+        16392: 'RB / RE / NEG'
+    }.get(productclass, 'Unknown')
