911f379b272fe541d3546575d6b654ba9dbf86b9
mkzero <info@linux-web-development.de>
2015-09-05 02:47:19 +0200

    Added elasticsearch import to trainspotter.sh

--- trainspotter.sh
+++ trainspotter.sh
@@ -3,9 +3,17 @@
 HDIR=/home/trainspotter/
 ID=$RANDOM
 
-echo "Starting trainspotter (id:$ID)" $(/bin/date) >> $HDIR/log/trainspotter-times.log 
-{ /usr/bin/time /usr/bin/python3 $HDIR/trainspotter/trainspotter.py \
-    1>$HDIR/log/trainspotter/alltrains-$(date '+%Y-%m-%d-%H-%M-%S').log \
-    2>>$HDIR/log/trainspotter/error.log; } 2>&1 >>$HDIR/log/trainspotter-times.log
+FOLDER=/home/trainspotter/log/trainspotter/trainlog-$(date '+%Y')/$(date '+%m')/$(date '+%d')
+DATE=$(date '+%Y-%m-%d-%H-%M-%S')
+
+echo "Starting trainspotter (id:$ID)" $(/bin/date) >> $HDIR/log/trainspotter-times.log
+mkdir -p $FOLDER
+{ \
+	/usr/bin/time /usr/bin/python3 /home/trainspotter/trainspotter/trainspotter.py \
+		1>$FOLDER/alltrains-$DATE.log \
+	2>>/home/trainspotter/log/trainspotter/error.log ; \
+} 2>&1 >>/home/trainspotter/log/trainspotter-times.log
 echo "Trainspotter stopped (id:$ID)" $(/bin/date) >> $HDIR/log/trainspotter-times.log
 
+cd /home/trainspotter/trainspotter/
+./import_to_es.py $FOLDER
